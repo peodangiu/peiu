580ea5a7f2ab3b20de77af2b1451b5b26378378f
peodangiu <86923720+peodangiu@users.noreply.github.com>
2021-07-05 21:22:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 const CONFIG = {
     introTitle: 'Peo à!',
-    introDesc: `Trái đất vốn lạ thường
+    introDesc: `                     Trái đất vốn lạ thường
                                       
-    Mà sao em cứ đi nhầm đường
+                                     Mà sao em cứ đi nhầm đường
                                        
-    Lạc vào tim anh lẻ loi
+                                     Lạc vào tim anh lẻ loi
                                        
-    Đằng sau chữ yêu đây là thương`,
+                                     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh mơ à :3',
+    btnNo: 'Anh mơ à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Tại vì anh đẹp zaiiii <33333333',
-    mess: 'Anh biết mà 🥰. I love you 3000 😘😘',
+    reply: 'Yêu thì yêu mà không yêu thì yêuuuuu <33333333',
+    mess: 'Anh biết mà 🥰. Love you 3000 😘😘',
     messDesc: 'Còn chờ gì nữa mà không inbox cho anh đi nàooooo.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/tu.taii.nb' //link mess của các bạn. VD: https://m.me/nam.nodemy
