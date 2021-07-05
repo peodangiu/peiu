7a21fef6d65fb2a5c159a4effb909bef8eabb816
peodangiu <86923720+peodangiu@users.noreply.github.com>
2021-07-05 21:38:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
     introTitle: 'Peo à!',
-    introDesc: `                     Trái đất vốn lạ thường ️🎶🎶🎶
+    introDesc: `                     Trái đất vốn lạ thường 🎵
                                       
-                                     Mà sao em cứ đi nhầm đường️ 🎶🎶🎶
+                                     Mà sao em cứ đi nhầm đường️ 🎵
                                        
-                                     Lạc vào tim anh lẻ loi️ 🎶🎶🎶
+                                     Lạc vào tim anh lẻ loi️ 🎵
                                        
-                                     Đằng sau chữ yêu đây là thương️ 🎶️🎶️🎶`,
+                                     Đằng sau chữ yêu đây là thương️ 🎵`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnYes: 'Vẫn cứ là yêu anh <33',
     btnNo: 'Anh mơ à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
